e82eb29ca0e32c5d9f866b6a42dee0a9182228f5
Dmitriy Afonichkin <dafonichkin@gmail.com>
2023-06-13 00:38:54 +0300

    3. Filter [#504865  [#504865]]

--- src/main/java/ru/job4j/dreamjob/controller/UserController.java
+++ src/main/java/ru/job4j/dreamjob/controller/UserController.java
@@ -42,12 +42,7 @@ public class UserController {
 
     @GetMapping("login")
     public String getLoginPage(Model model, HttpSession session) {
-        var user = (User) session.getAttribute("user");
-        if (user == null) {
-            user = new User();
-            user.setName("Гость");
-        }
-        model.addAttribute("user", user); return "users/login";
+        return "users/login";
     }
 
     @PostMapping("login")
